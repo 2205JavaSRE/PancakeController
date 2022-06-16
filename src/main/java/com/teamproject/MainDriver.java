6dbc433a78e8f7a261532032ac5c78623024bbea
Jason Maggart <jmaggart1@gmail.com>
2022-06-15 23:50:19 -0400

    jenkinstest

--- src/main/java/com/teamproject/MainDriver.java
+++ src/main/java/com/teamproject/MainDriver.java
@@ -20,23 +20,22 @@ public class MainDriver {
 
 	public static void main(String...args) {
 
-		PrometheusMeterRegistry registry = MicrometerMonitorController.StartMonitoringRegistry();
+
 		Javalin app = Javalin.create( config -> {
-			config.registerPlugin(new MicrometerPlugin(registry));
+			config.registerPlugin(new MicrometerPlugin(Prometheus.registry));
 				}
 				).start(7070);
 		
 		 RequestMapping.configureRoutes(app);
 	
-		 MicrometerMonitorController.MoniteringPaths(app, registry);
+		
 		
 		 
 			
-//		 app.get("/prometheus", ctx -> {
-//				 
-//				 ctx.result(Prometheus.registry.scrape());
-//			 });
-		
+		 app.get("/prometheus", ctx -> {
+				 
+				 ctx.result(Prometheus.registry.scrape());
+			 });
 	}
 	
 	 
